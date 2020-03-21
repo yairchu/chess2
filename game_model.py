dd3c141b2fa19cd2bf44f27cd34cb9151e8d2dec
Yair Chuchem <yairchu@gmail.com>
2020-03-22 00:16:49 +0200

    oops black didnt get points due to mutability bug

--- game_model.py
+++ game_model.py
@@ -23,9 +23,9 @@ class GameModel:
         self.num_players = num_boards * 2
         for who, (x, y0, y1) in enumerate([(0, 0, 1), (0, 7, 6), (8, 0, 1), (8, 7, 6)][:self.num_players]):
             for dx, piece in enumerate(chess.first_row):
-                piece(who, (x+dx, y0), self)
+                p = piece(who, (x+dx, y0), self)
                 if piece == chess.King:
-                    piece.on_die = lambda x: self.king_captured(who)
+                    p.on_die = lambda who=who: self.king_captured(who)
                 chess.Pawn(who, (x+dx, y1), self)
 
     def in_bounds(self, pos):
