yairchu/chess2

commit c71741eb8bf3d38c792952358f7abc23928a70d8

surrender feature

main.py
@@ -153,11 +153,11 @@ class Game(BoxLayout):
                 return
             self.game_model.add_action(*command[1:].split())
             return
-        if self.game_model.active():
-            # Chat
-            self.game_model.add_action('msg', command)
+        if self.game_model.mode is None:
+            self.net_engine.connect(command)
             return
-        self.net_engine.connect(command)
+        # Chat
+        self.game_model.add_action('msg', command)
 
     def king_captured(self, who):
         if self.game_model.mode == 'replay':

game_model.py
@@ -89,8 +89,12 @@ class GameModel:
                 self.add_message(self.tutorial_messages.pop(0))
     action_move.quiet = True
 
-    def action_reset(self, _id, num_boards=None):
-        self.init(None if num_boards is None else int(num_boards))
+    def action_surrender(self, i):
+        player = self.players.get(i)
+        if player is None:
+            return
+        self.add_message(self.nick(i) + ' surrendered')
+        self.king_captured(player)
 
     def player_str(self, player):
         if player is None:
@@ -121,4 +125,4 @@ class GameModel:
         self.nicknames[i] = nick
 
     def help(self):
-        self.add_message('commands: /help | /nick <name> | /reset | /credits')
+        self.add_message('commands: /help | /nick <name> | /surrender | /credits')

commit 43a5ddecb3bfe6f566e5ffe9ea41edb52a09f1cd

oops broke ability to connect in c71741eb8bf

main.py
@@ -153,7 +153,7 @@ class Game(BoxLayout):
                 return
             self.game_model.add_action(*command[1:].split())
             return
-        if self.game_model.mode is None:
+        if self.game_model.mode in [None, 'connect']:
             self.net_engine.connect(command)
             return
         # Chat